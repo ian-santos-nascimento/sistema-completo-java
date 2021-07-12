0324fa96a0e5bbf0c63971ee4802fa7bc0c22447
Ian Santos <iansantosnascimento@gmail.com>
2021-07-12 18:42:04 -0300

    Projeto criado

---
+++ src/test/java/com/example/java_udemy/JavaUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
